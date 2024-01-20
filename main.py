2cfe8a254cd6469f306513e9665cba295d56281d
ishika <shukla22164024@akgec.ac.in>
2024-01-20 17:04:20 +0530

    h

--- main.py
+++ main.py
@@ -64,4 +64,4 @@ def products_in_category(category: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
